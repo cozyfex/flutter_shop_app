3760b1d2eb90261fb7cebf6d2622e45e94ccf398
Felix D.H. Kang <cozyfex@gmail.com>
2020-10-21 19:26:29 +0900

    Local State

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -38,23 +38,27 @@ class Products with ChangeNotifier {
     ),
   ];
 
-  var _showFavoritesOnly = false;
+  // var _showFavoritesOnly = false;
 
-  void showFavoritesOnly() {
-    _showFavoritesOnly = true;
-    notifyListeners();
-  }
+  // void showFavoritesOnly() {
+  //   _showFavoritesOnly = true;
+  //   notifyListeners();
+  // }
 
-  void showAll() {
-    _showFavoritesOnly = false;
-    notifyListeners();
-  }
+  // void showAll() {
+  //   _showFavoritesOnly = false;
+  //   notifyListeners();
+  // }
 
   List<Product> get items {
-    if (_showFavoritesOnly) {
-      return _items.where((element) => element.isFavorite).toList();
-    }
+    // if (_showFavoritesOnly) {
+    //   return _items.where((element) => element.isFavorite).toList();
+    // }
     return [..._items];
+  }
+
+  List<Product> get favoritesItems {
+    return _items.where((element) => element.isFavorite).toList();
   }
 
   Product findById(String id) {
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../providers/products.dart';
 
 import '../widgets/products_grid.dart';
 
@@ -10,39 +7,47 @@ enum FilterOptions {
   All,
 }
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
-    final productsContainer = Provider.of<Products>(context, listen: false);
-
     return Scaffold(
-        appBar: AppBar(
-          title: Text('MyShop'),
-          actions: [
-            PopupMenuButton(
-              icon: Icon(
-                Icons.more_vert,
-              ),
-              onSelected: (FilterOptions selectedValue) {
+      appBar: AppBar(
+        title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
                 if (selectedValue == FilterOptions.Favorites) {
-                  productsContainer.showFavoritesOnly();
+                  _showOnlyFavorites = true;
                 } else {
-                  productsContainer.showAll();
+                  _showOnlyFavorites = false;
                 }
-              },
-              itemBuilder: (_) => [
-                PopupMenuItem(
-                  child: Text('Only Favorites!'),
-                  value: FilterOptions.Favorites,
-                ),
-                PopupMenuItem(
-                  child: Text('Show All!'),
-                  value: FilterOptions.All,
-                ),
-              ],
-            )
-          ],
-        ),
-        body: ProductsGrid());
+              });
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites!'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All!'),
+                value: FilterOptions.All,
+              ),
+            ],
+          )
+        ],
+      ),
+      body: ProductsGrid(_showOnlyFavorites),
+    );
   }
 }

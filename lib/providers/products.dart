34052f1f26955f01bb8f357bd62d55039be7c92f
Felix D.H. Kang <cozyfex@gmail.com>
2020-10-21 19:20:55 +0900

    App-wide State

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -38,7 +38,22 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  var _showFavoritesOnly = false;
+
+  void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
+  }
+
   List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((element) => element.isFavorite).toList();
+    }
     return [..._items];
   }
 
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../providers/product.dart';
+import '../providers/products.dart';
 
 import '../widgets/products_grid.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
+
     return Scaffold(
         appBar: AppBar(
           title: Text('MyShop'),
+          actions: [
+            PopupMenuButton(
+              icon: Icon(
+                Icons.more_vert,
+              ),
+              onSelected: (FilterOptions selectedValue) {
+                if (selectedValue == FilterOptions.Favorites) {
+                  productsContainer.showFavoritesOnly();
+                } else {
+                  productsContainer.showAll();
+                }
+              },
+              itemBuilder: (_) => [
+                PopupMenuItem(
+                  child: Text('Only Favorites!'),
+                  value: FilterOptions.Favorites,
+                ),
+                PopupMenuItem(
+                  child: Text('Show All!'),
+                  value: FilterOptions.All,
+                ),
+              ],
+            )
+          ],
         ),
         body: ProductsGrid());
   }

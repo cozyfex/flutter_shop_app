59dd43a75eb9c085161e1e4102e78d7b2a2f5813
Felix D.H. Kang <cozyfex@gmail.com>
2020-10-26 13:04:42 +0900

    Saving New Products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -65,8 +65,16 @@ class Products with ChangeNotifier {
     return _items.firstWhere((element) => element.id == id);
   }
 
-  void addProduct() {
-    // _items.add();
+  void addProduct(Product product) {
+    final newProduct = Product(
+      title: product.title,
+      price: product.price,
+      description: product.description,
+      imageUrl: product.imageUrl,
+      id: DateTime.now().toString(),
+    );
+    _items.add(newProduct);
+    // _items.insert(0, newProduct); // at the start of the list
     notifyListeners();
   }
 }
